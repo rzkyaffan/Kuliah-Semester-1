8f41ef975442cfa6f7ea1320b4f38831b484c25d
affan <affandirrh@gmail.com>
2026-09-16 15:16:48 +0700

    Menambahkan file variabel.java

---
+++ latihanMandiri/variable.java
@@ -0,0 +1,21 @@
+package latihanMandiri; 
+
+public class variable {
+    public static void main(String[] args){
+        // Beberapa cara melakukan deklarasi variabel di java 
+
+        //===Cara pertama===//
+        
+        int a = 8; 
+        System.out.println(a); //=> akan menghasilkan bilangan 8
+        
+        //===Cara kedua===//
+        int var1,var2,var3; 
+        var1 = 8;
+        var2 = 9;
+        var3 = 11; 
+        System.out.println(var1);
+
+
+    }
+}
